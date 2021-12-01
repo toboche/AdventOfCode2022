d32f920a8c85285bfa14a4b3925b7fdd75703486
toboche <toboche@gmail.com>
2021-12-01 17:39:04 +0100

    init

---
+++ src/main/kotlin/pl/toboche/Day1.kt
@@ -0,0 +1,7 @@
+package pl.toboche
+
+class Day1 {
+    fun task1(input: List<String>): Long {
+        return 0
+    }
+}
---
+++ src/test/kotlin/pl/toboche/Day1Test.kt
@@ -0,0 +1,10 @@
+package pl.toboche
+
+import org.junit.jupiter.api.Test
+
+internal class Day1Test {
+    @Test
+    internal fun task1Example() {
+
+    }
+}
