b35f49cbeb4af22011115b028aa1a832bc8b0a80
toboche <toboche@gmail.com>
2021-12-03 21:02:26 +0100

    day 2 task 2 example ugly

--- src/main/kotlin/pl/toboche/Day3.kt
+++ src/main/kotlin/pl/toboche/Day3.kt
@@ -2,13 +2,7 @@ package pl.toboche
 
 class Day3 {
     fun task1(input: List<String>): Int {
-        val bits = input[0].length
-        val ones = IntArray(bits)
-        input.forEach {
-            it.forEachIndexed { index, c ->
-                ones[index] += if (c == '1') 1 else 0
-            }
-        }
+        val ones = countOnes(input)
         val halfOfValues = input.size / 2
         return replaceMostCommonOccurrences(ones,
             halfOfValues,
@@ -20,8 +14,69 @@ class Day3 {
                     '1')
     }
 
+    private fun countOnes(input: List<String>): IntArray {
+        val bits = input[0].length
+        val ones = IntArray(bits)
+        input.forEach {
+            it.forEachIndexed { index, c ->
+                ones[index] += if (c == '1') 1 else 0
+            }
+        }
+        return ones
+    }
+
+    private fun findMostCommonValue(input: List<List<Boolean>>, index: Int): Boolean {
+        var ones = 0
+        input.forEach {
+            ones += if (it[index]) 1 else 0
+        }
+        val halfOfValues = input.size / 2
+
+        return if (halfOfValues * 2 == input.size && ones == halfOfValues) {
+            true
+        } else {
+            ones > halfOfValues
+        }
+    }
+
+    private fun findLeastCommonValue(input: List<List<Boolean>>, index: Int): Boolean {
+        var ones = 0
+        input.forEach {
+            ones += if (it[index]) 1 else 0
+        }
+        val halfOfValues = input.size / 2
+
+        return if (halfOfValues * 2 == input.size && ones == halfOfValues) {
+            false
+        } else {
+            ones <= halfOfValues
+        }
+    }
+
     private fun replaceMostCommonOccurrences(ones: IntArray, halfOfValues: Int, mostCommon: Char, leastCommon: Char) =
         ones.map { if (it > halfOfValues) mostCommon else leastCommon }
             .joinToString("")
             .toInt(2)
+
+    fun task2(input: List<String>): Int {
+        return filterForCondition(input, ::findLeastCommonValue) * filterForCondition(input, ::findMostCommonValue)
+    }
+
+    private fun filterForCondition(
+        input: List<String>,
+        filteringCondition: (input: List<List<Boolean>>, index: Int) -> Boolean,
+    ): Int {
+        var filtered = input.map { it.map { it == '1' } }
+        var iter = 0
+        while (filtered.size != 1) {
+            val mostCommonValue = filteringCondition(filtered, iter)
+            filtered = filtered.filter { it[iter] == mostCommonValue }
+            iter++
+        }
+        return filtered.first()
+            .map { if (it) '1' else '0' }
+            .joinToString("")
+            .toInt(2)
+    }
+
 }
--- src/test/kotlin/pl/toboche/Day3Test.kt
+++ src/test/kotlin/pl/toboche/Day3Test.kt
@@ -33,4 +33,24 @@ internal class Day3Test {
         assertEquals(3895776,
             Day3().task1(input))
     }
+
+    @Test
+    internal fun task2Example() {
+        val input = ("00100\n" +
+                "11110\n" +
+                "10110\n" +
+                "10111\n" +
+                "10101\n" +
+                "01111\n" +
+                "00111\n" +
+                "11100\n" +
+                "10000\n" +
+                "11001\n" +
+                "00010\n" +
+                "01010")
+            .lines()
+
+        assertEquals(230,
+            Day3().task2(input))
+    }
 }
