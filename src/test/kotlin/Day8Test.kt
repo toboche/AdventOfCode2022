e92f7a70034c9362707de9e39dfb7ec4bf62b8a4
toboche <toboche@gmail.com>
2021-12-08 21:42:32 +0100

    day 8 task 1 solved

--- src/test/kotlin/Day8Test.kt
+++ src/test/kotlin/Day8Test.kt
@@ -3,10 +3,18 @@ import org.junit.jupiter.api.Test
 import java.io.File
 
 internal class Day8Test {
-
     @Test
     internal fun task1Example() {
         val input = File("src/test/resources/day8Example1.txt")
+            .readLines()
+
+        assertEquals(26,
+            Day8().task1(input))
+    }
+
+    @Test
+    internal fun task1() {
+        val input = File("src/test/resources/day8Task1.txt")
             .readLines()
 
         assertEquals(26,
