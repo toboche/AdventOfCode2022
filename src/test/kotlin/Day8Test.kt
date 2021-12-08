db1d0c6aed5a900f983d94e6598c9958eb7b9a82
toboche <toboche@gmail.com>
2021-12-08 22:39:36 +0100

    day 8 task 2 example

--- src/test/kotlin/Day8Test.kt
+++ src/test/kotlin/Day8Test.kt
@@ -17,7 +17,15 @@ internal class Day8Test {
         val input = File("src/test/resources/day8Task1.txt")
             .readLines()
 
-        assertEquals(26,
+        assertEquals(303,
             Day8().task1(input))
+    }
+
+    @Test
+    internal fun task2Example1() {
+        val input = listOf("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+
+        assertEquals(5353,
+            Day8().task2(input))
     }
 }
