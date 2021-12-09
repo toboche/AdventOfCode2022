178851607ae29ccacc92dc3a2cfc0d64e6adb141
toboche <toboche@gmail.com>
2021-12-09 19:18:36 +0100

    day 9 task 2 example

--- src/main/kotlin/pl/toboche/Day9.kt
+++ src/main/kotlin/pl/toboche/Day9.kt
@@ -15,25 +15,61 @@ class Day9 {
         val yBounds = array.indices
         val xBounds = array[0].indices
 
-        return findLocalMinimas(array, xBounds, yBounds)
+        return findLocalMinima(array, xBounds, yBounds)
             .sumOf { it.sumOf { it.value + 1 } }
     }
 
-    private fun findLocalMinimas(
+    private fun findLocalMinima(
         array: Array<IntArray>,
         xBounds: IntRange,
         yBounds: IntRange,
     ) = array.mapIndexed { y, ints ->
         ints.withIndex().filter { (x, value) ->
-            ds.map { (dx, dy) ->
-                x + dx to y + dy
-            }.filter { (nx, ny) ->
-                xBounds.contains(nx) && yBounds.contains(ny)
-            }.all { (nx, ny) -> array[ny][nx] > value }
+            allNeighbors(x, y, xBounds, yBounds)
+                .all { (nx, ny) -> array[ny][nx] > value }
         }
     }
 
+    private fun allNeighbors(
+        x: Int,
+        y: Int,
+        xBounds: IntRange,
+        yBounds: IntRange,
+    ) = ds.map { (dx, dy) ->
+        x + dx to y + dy
+    }.filter { (nx, ny) ->
+        xBounds.contains(nx) && yBounds.contains(ny)
+    }
+
     fun task2(input: List<String>): Int {
-        return -1
+        val array = input.map { it.map { it.digitToInt() }.toIntArray() }.toTypedArray()
+
+        val yBounds = array.indices
+        val xBounds = array[0].indices
+
+        return findLocalMinima(array, xBounds, yBounds).withIndex()
+            .map { (y, minima) ->
+                minima.map { (x, minimum) ->
+                    val visited = mutableSetOf<Pair<Int, Int>>()
+                    val convergent = mutableSetOf<Pair<Int, Int>>()
+                    val toCheck = mutableListOf<Pair<Int, Int>>()
+                    toCheck.add(x to y)
+                    while (toCheck.isNotEmpty()) {
+                        val current = toCheck.removeFirst()
+                        visited.add(current)
+                        if (array[current.second][current.first] != 9) {
+                            convergent.add(current)
+                            toCheck
+                                .addAll(allNeighbors(current.first, current.second, xBounds, yBounds) - visited)
+                        }
+                    }
+                    convergent.count()
+                }
+            }.flatten()
+            .sorted()
+            .reversed()
+            .let {
+                it[0] * it[1] * it[2]
+            }
     }
 }
