c146d2724cc47d1dfaa8713faf31b1f2d093e1cc
toboche <toboche@gmail.com>
2021-12-11 10:16:51 +0100

    day 11 task 2 example

--- src/test/kotlin/pl/toboche/Day11Test.kt
+++ src/test/kotlin/pl/toboche/Day11Test.kt
@@ -75,4 +75,13 @@ internal class Day11Test {
         assertEquals(1719,
             Day11().task1(input, steps = 100))
     }
+
+    @Test
+    internal fun task2ExampleFull() {
+        val input = example
+            .lines()
+
+        assertEquals(195,
+            Day11().task2(input))
+    }
 }
