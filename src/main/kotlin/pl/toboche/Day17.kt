ab9f20185bec193ce0e1f83f438082b0f720bab0
toboche <toboche@gmail.com>
2021-12-18 11:16:39 +0100

    day 17 task 2 solved

--- src/main/kotlin/pl/toboche/Day17.kt
+++ src/main/kotlin/pl/toboche/Day17.kt
@@ -5,6 +5,10 @@ import kotlin.math.max
 
 class Day17 {
     fun task1(input: String): Int {
+        return solve(input).maxByOrNull { it.third }!!.third
+    }
+
+    fun solve(input: String): List<Triple<Int, Int, Int>> {
         val (xmin, xmax) = input.substringAfter("target area: x=")
             .split(", ")[0]
             .split("..")
@@ -17,18 +21,18 @@ class Day17 {
         val yrange = ymin..ymax
 
 
-        var highest = Int.MIN_VALUE
+        val solutions = mutableListOf<Triple<Int, Int, Int>>()
 
         for (xvel in 1..xmax) {
             for (yvel in ymin..ymin.absoluteValue) {
                 val current = simulate(xvel, yvel, xrange, yrange)
-                if (current != Int.MIN_VALUE) {
-                    highest = max(highest, current)
+                if (current != null) {
+                    solutions.add(Triple(xvel, yvel, current))
                 }
             }
         }
 
-        return highest
+        return solutions
     }
 
     private fun simulate(
@@ -36,24 +40,21 @@ class Day17 {
         yvel: Int,
         xrange: IntRange,
         yrange: IntRange,
-    ): Int {
+    ): Int? {
         var xvel1 = xvel
         var yvel1 = yvel
         var x = 0
         var y = 0
         var highest = Int.MIN_VALUE
-        var targetReached = false
-        while (x <= xrange.last && y >= yrange.last) {
+        while (x <= xrange.last && y >= yrange.first) {
             x += xvel1
-            if (xvel1 > 0) xvel1 -= 1 else if (xvel1 < 0) xvel1 += 1
-            y += yvel1
-            yvel1 -= 1
+            y += yvel1--
+            if (xvel1 > 0) xvel1 -= 1 else xvel1 = 0
             highest = max(highest, y)
             if (xrange.contains(x) && yrange.contains(y)) {
-                targetReached = true
-                break
+                return highest
             }
         }
-        return if (targetReached) highest else Int.MIN_VALUE
+        return null
     }
 }
