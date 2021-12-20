5e0fe5ec6b09c262c129867a9a1622f00671d1c7
toboche <toboche@gmail.com>
2021-12-20 23:11:28 +0100

    day 20 task 2 solved

--- src/test/kotlin/pl/toboche/Day20Test.kt
+++ src/test/kotlin/pl/toboche/Day20Test.kt
@@ -20,8 +20,25 @@ internal class Day20Test {
             .readLines()
 
         Assertions.assertThat(
-            Day20().task1(input, 2)).isEqualTo(35)
+            Day20().task1(input, 2)).isEqualTo(5479)
+    }
+    Ļ
+    @Test
+    internal fun task2Example() {
+        val input = File("src/test/resources/day20Example.txt")
+            .readLines()
+
+        Assertions.assertThat(
+            Day20().task1(input, 50)).isEqualTo(3351)
     }
 
 
+    @Test
+    internal fun task2() {
+        val input = File("src/test/resources/day20.txt")
+            .readLines()
+        ŻĻ
+        Assertions.assertThat(
+            Day20().task1(input, 50)).isEqualTo(19012)
+    }
 }
