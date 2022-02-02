e55ac4a45d4c6c65209d3d9ca9de80da4d0e30c2
toboche <toboche@gmail.com>
2022-02-02 20:16:15 +0100

    Day 22 part 1 solved

--- src/main/kotlin/pl/toboche/Day22.kt
+++ src/main/kotlin/pl/toboche/Day22.kt
@@ -1,14 +1,14 @@
 package pl.toboche
 
+import java.lang.Integer.max
+import java.lang.Integer.min
+
 class Day22 {
     fun task1(input: String): Int {
         val cubes = mutableSetOf<Triple<Int, Int, Int>>()
         input.lines().forEach { line ->
             val splitBySpace = line.split(" ")
             val splitByComma = splitBySpace[1].split(",")
-//            Cuboid(
-//                split[0] == "on", range(split[1]), range(split[2]), range(split[3])
-//            )
             range(splitByComma[0]).flatMap { x -> range(splitByComma[1]).map { y -> x to y } }
                 .flatMap { xToY -> range(splitByComma[2]).map { Triple(xToY.first, xToY.second, it) } }
                 .forEach {
@@ -23,7 +23,21 @@ class Day22 {
     }
 
     private fun range(s: String): IntRange {
-        val (x1, x2) = s.drop(2).split("..").map { it.toInt() }
+        val (x1parsed, x2parsed) = s.drop(2).split("..").map { it.toInt() }
+        val x1 = if (x1parsed < -50) {
+            -50
+        } else if (x1parsed > 50) {
+            return IntRange.EMPTY
+        } else {
+            x1parsed
+        }
+        val x2 = if (x2parsed > 50) {
+            50
+        } else if (x2parsed < -50)
+            return IntRange.EMPTY
+        else {
+            x2parsed
+        }
         return x1..x2
     }
 
