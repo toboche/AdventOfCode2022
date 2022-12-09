b3aaa5f2556e6e0d7341d6d50f96d71f03b5baeb
toboche <toboche@gmail.com>
2022-12-09 18:50:17 +0100

    day 8 task 2 - wip

--- src/main/kotlin/pl/toboche/Day8.kt
+++ src/main/kotlin/pl/toboche/Day8.kt
@@ -9,20 +9,10 @@ class Day8 {
     fun task1(input: List<String>, findMin: Boolean = false): Int {
         val visible = Array(input.size) { BooleanArray(input[0].length) }
 
-        traverseHorizontally(
-            input,
-            visible,
-            0 until input.size,
-            0 until input[0].length
-        ) { curr, max, i, j -> analyse(curr, max, visible, i, j) }
-        traverseHorizontally(
-            input,
-            visible,
-            input.size - 1 downTo 0,
-            input[0].length - 1 downTo 1
-        ) { curr, max, i, j -> analyse(curr, max, visible, i, j) }
-        traverseVertically(input, visible, 0 until input[0].length, 0 until input.size){ curr, max, i, j -> analyse(curr, max, visible, i, j) }
-        traverseVertically(input, visible, input[0].length - 1 downTo 0, input.size - 1 downTo 0){ curr, max, i, j -> analyse(curr, max, visible, i, j) }
+        traverseHorizontally(input, visible, 0 until input.size, 0 until input[0].length)
+        traverseHorizontally(input, visible, input.size - 1 downTo 0, input[0].length - 1 downTo 1)
+        traverseVertically(input, visible, 0 until input[0].length, 0 until input.size)
+        traverseVertically(input, visible, input[0].length - 1 downTo 0, input.size - 1 downTo 0)
         return visible.sumOf { it.count { it } }
     }
 
@@ -30,29 +20,18 @@ class Day8 {
         input: List<String>,
         visible: Array<BooleanArray>,
         outerLoopRange: IntProgression,
-        innerLoopRange: IntProgression,
-        onItemAction: (Int, Int, Int, Int) -> Unit
+        innerLoopRange: IntProgression
     ) {
         for (i in outerLoopRange) {
             val line = input[i]
             var max = -1
             for (j in innerLoopRange) {
                 val curr = line[j].digitToInt()
-                onItemAction(curr, max, i, j)
+                if (curr > max) {
+                    visible[i][j] = true
+                }
                 max = max(max, curr)
             }
-        }
-    }
-
-    private fun analyse(
-        curr: Int,
-        max: Int,
-        visible: Array<BooleanArray>,
-        i: Int,
-        j: Int
-    ) {
-        if (curr > max) {
-            visible[i][j] = true
         }
     }
 
@@ -60,16 +39,119 @@ class Day8 {
         input: List<String>,
         visible: Array<BooleanArray>,
         outerLoopRange: IntProgression,
-        innerLoopRange: IntProgression,
-        onItemAction: (Int, Int, Int, Int) -> Unit
+        innerLoopRange: IntProgression
     ) {
         for (j in outerLoopRange) {
             var max = -1
             for (i in innerLoopRange) {
                 val curr = input[i][j].digitToInt()
-                onItemAction(curr, max, i, j)
+                if (curr > max) {
+                    visible[i][j] = true
+                }
                 max = max(max, curr)
             }
         }
     }
+
+
+    fun task2(input: List<String>, findMin: Boolean = false): Int {
+        val visible = Array(input.size) { IntArray(input[0].length) { _ -> 1 } }
+
+        for (i in input.indices) {
+            val line = input[i]
+            for (j in line.indices) {
+                val curr = line[j]
+                if (j == 0 || i == 0 || i == line.count() - 1 || j == input.count() - 1) {
+                    visible[i][j] = 0
+                    continue
+                }
+                var max = 1
+                for (k in i - 1 downTo 0) {
+                    if (input[k][j] >= curr) {
+                        max = max * (i - k)
+                        break
+                    }
+                }
+                for (k in i +1 until input.count()) {
+                    if (input[k][j] >= curr) {
+                        max = max * (k-i)
+                        break
+                    }
+                }
+                for (k in j - 1 downTo 0) {
+                    if (input[i][k] >= curr) {
+                        max = max * (j - k)
+                        break
+                    }
+                }
+                for (k in j +1 until line.count()) {
+                    if (input[i][k] >= curr) {
+                        max = max * (k - j)
+                        break
+                    }
+                }
+                visible[i][j] = max
+            }
+        }
+        return visible.maxOf { it.maxOrNull()!! }
+    }
+
+//    fun task2(input: List<String>, findMin: Boolean = false): Int {
+//        val visible = Array(input.size) { IntArray(input[0].length) { _ -> 1 } }
+//
+//        traverseVisibilityHorizontally(input, visible, 0 until input.size, 0 until input[0].length)
+////        traverseVisibilityHorizontally(input, visible, input.size - 1 downTo 0, input[0].length - 1 downTo 1)
+//        traverseVisibilityVertically(input, visible, 0 until input[0].length, 0 until input.size)
+////        traverseVisibilityVertically(input, visible, input[0].length - 1 downTo 0, input.size - 1 downTo 0)
+//        return visible.maxOf { it.maxOrNull()!! }
+//    }
+
+    fun traverseVisibilityHorizontally(
+        input: List<String>,
+        visible: Array<IntArray>,
+        outerLoopRange: IntProgression,
+        innerLoopRange: IntProgression
+    ) {
+        for (i in outerLoopRange) {
+            val line = input[i]
+            val lastVisitedHeightIndices = IntArray(10) { -1 }
+            var max = -1
+            for (j in innerLoopRange) {
+                val curr = line[j].digitToInt()
+                val currVisibility = (curr..9).map { lastVisitedHeightIndices[it] }
+                    .filter { it != -1 }
+                    .maxOrNull()
+                    ?.let { j - it } ?: j
+                visible[i][j] = visible[i][j] * currVisibility
+
+                max = max(max, curr)
+                lastVisitedHeightIndices[curr] = j
+            }
+        }
+    }
+
+    fun traverseVisibilityVertically(
+        input: List<String>,
+        visible: Array<IntArray>,
+        outerLoopRange: IntProgression,
+        innerLoopRange: IntProgression
+    ) {
+        for (j in outerLoopRange) {
+            val lastVisitedHeightIndices = IntArray(10) { -1 }
+            var max = -1
+            for (i in innerLoopRange) {
+                val curr = input[i][j].digitToInt()
+                val currVisibility = (curr..9).map { lastVisitedHeightIndices[it] }
+                    .filter { it != -1 }
+                    .maxOrNull()
+                    ?.let { j - it } ?: i
+                visible[i][j] = visible[i][j] * currVisibility
+
+                max = max(max, curr)
+                lastVisitedHeightIndices[curr] = j
+            }
+        }
+    }
+
+
 }
--- src/test/kotlin/pl/toboche/Day8Test.kt
+++ src/test/kotlin/pl/toboche/Day8Test.kt
@@ -1,5 +1,6 @@
 package pl.toboche
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -28,6 +29,33 @@ class Day8Test {
         assertEquals(
             1812,
             Day8().task1(input)
+        )
+    }
+
+    @Test
+    fun task2ExampleSingleLineSingleRun() {
+        val input = listOf("30373")
+        val visible = Array(input.size) { IntArray(input[0].length) { _ -> 1 } }
+
+        Day8().traverseVisibilityHorizontally(input, visible, 0 until input.size, 0 until input[0].length)
+
+        assertArrayEquals(
+            Array(1) { intArrayOf(0, 1, 2, 3, 1) },
+            visible
+        )
+    }
+
+    @Test
+    fun task2Example() {
+        val input = ("30373\n" +
+                "25512\n" +
+                "65332\n" +
+                "33549\n" +
+                "35390")
+            .lines()
+        assertEquals(
+            8,
+            Day8().task2(input)
         )
     }
 }
